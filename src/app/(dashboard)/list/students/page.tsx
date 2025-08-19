3d9ddff91317f557ba91ebd510c77c96e702bed6
Limon00001 <limonhossain82@gmail.com>
2025-08-19 15:16:47 +0600

    feat(students): add student listing table

---
+++ src/app/(dashboard)/list/students/page.tsx
@@ -0,0 +1,150 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 19 Aug, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+// External Imports
+import {
+  ArrowDownWideNarrow,
+  Eye,
+  Plus,
+  SlidersHorizontal,
+  Trash,
+} from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+// Internal Imports
+import Pagination from '@/components/Pagination';
+import Table from '@/components/Table';
+import TableSearch from '@/components/TableSearch';
+import { role, studentsData } from '@/lib/data';
+
+// Types
+type Student = {
+  id: number;
+  studentId: string;
+  name: string;
+  email?: string;
+  photo: string;
+  phone?: string;
+  grade: number;
+  class: string;
+  address: string;
+};
+
+// Data
+const columns = [
+  {
+    header: 'Info',
+    accessor: 'info',
+  },
+  {
+    header: 'Student ID',
+    accessor: 'studentId',
+    className: 'hidden md:table-cell',
+  },
+  {
+    header: 'Grade',
+    accessor: 'grade',
+    className: 'hidden md:table-cell',
+  },
+  {
+    header: 'Phone',
+    accessor: 'phone',
+    className: 'hidden lg:table-cell',
+  },
+  {
+    header: 'Address',
+    accessor: 'address',
+    className: 'hidden lg:table-cell',
+  },
+  {
+    header: 'Actions',
+    accessor: 'action',
+  },
+];
+
+/**
+ * Teacher List Page
+ */
+const StudentListPage = () => {
+  // Render Table Row
+  const renderRow = (item: Student) => {
+    return (
+      <tr
+        key={item.id}
+        className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-appPurpleLight"
+      >
+        <td className="flex items-center gap-4 p-4">
+          <Image
+            src={item.photo}
+            alt=""
+            width={40}
+            height={40}
+            className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
+          />
+          <div className="flex flex-col">
+            <h3 className="font-semibold">{item.name}</h3>
+            <p className="text-xs text-gray-500">{item.class}</p>
+          </div>
+        </td>
+        <td className="hidden md:table-cell">{item.studentId}</td>
+        <td className="hidden md:table-cell">{item.grade}</td>
+        <td className="hidden lg:table-cell">{item.phone}</td>
+        <td className="hidden lg:table-cell">{item.address}</td>
+        <td>
+          <div className="flex items-center gap-2">
+            <Link href={`/list/teachers/${item.id}`}>
+              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-appSky cursor-pointer">
+                <Eye className="w-4 h-4" />
+              </button>
+            </Link>
+            {role === 'admin' && (
+              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-red-500/30 cursor-pointer">
+                <Trash className="w-4 h-4" />
+              </button>
+            )}
+          </div>
+        </td>
+      </tr>
+    );
+  };
+
+  return (
+    <div className="flex-1 bg-white p-4 rounded-md m-4 mt-0">
+      {/* TOP SECTION */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All Students</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          {/* SEARCH */}
+          <TableSearch />
+
+          {/* BUTTONS */}
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-appYellow cursor-pointer">
+              <SlidersHorizontal className="w-4 h-4" />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-appYellow cursor-pointer">
+              <ArrowDownWideNarrow className="w-4 h-4" />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-appYellow cursor-pointer">
+              <Plus className="w-4 h-4" />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* TABLE SECTION */}
+      <Table columns={columns} renderRow={renderRow} data={studentsData} />
+
+      {/* PAGINATION */}
+      <Pagination />
+    </div>
+  );
+};
+
+// Export
+export default StudentListPage;
