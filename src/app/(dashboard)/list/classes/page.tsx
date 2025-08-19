032969de72f6f8305755a89b4e64facc2d2ed751
Limon00001 <limonhossain82@gmail.com>
2025-08-19 15:47:26 +0600

    feat(classes): add class listing table

---
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -0,0 +1,127 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 19 Aug, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+// External Imports
+import {
+  ArrowDownWideNarrow,
+  Eye,
+  Plus,
+  SlidersHorizontal,
+  Trash,
+} from 'lucide-react';
+import Link from 'next/link';
+
+// Internal Imports
+import Pagination from '@/components/Pagination';
+import Table from '@/components/Table';
+import TableSearch from '@/components/TableSearch';
+import { classesData, role } from '@/lib/data';
+
+// Types
+type Class = {
+  id: number;
+  name: string;
+  capacity: number;
+  grade: number;
+  supervisor: string;
+};
+
+// Data
+const columns = [
+  {
+    header: 'Class Name',
+    accessor: 'name',
+  },
+  {
+    header: 'Capacity',
+    accessor: 'capacity',
+    className: 'hidden md:table-cell',
+  },
+  {
+    header: 'Grade',
+    accessor: 'grade',
+    className: 'hidden md:table-cell',
+  },
+  {
+    header: 'Supervisor',
+    accessor: 'supervisor',
+    className: 'hidden md:table-cell',
+  },
+  {
+    header: 'Actions',
+    accessor: 'action',
+  },
+];
+
+/**
+ * Class List Page
+ */
+const ClassListPage = () => {
+  // Render Table Row
+  const renderRow = (item: Class) => {
+    return (
+      <tr
+        key={item.id}
+        className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-appPurpleLight"
+      >
+        <td className="flex items-center gap-4 p-4">{item.name}</td>
+        <td className="hidden md:table-cell">{item.capacity}</td>
+        <td className="hidden md:table-cell">{item.grade}</td>
+        <td className="hidden md:table-cell">{item.supervisor}</td>
+        <td>
+          <div className="flex items-center gap-2">
+            <Link href={`/list/teachers/${item.id}`}>
+              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-appSky cursor-pointer">
+                <Eye className="w-4 h-4" />
+              </button>
+            </Link>
+            {role === 'admin' && (
+              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-red-500/30 cursor-pointer">
+                <Trash className="w-4 h-4" />
+              </button>
+            )}
+          </div>
+        </td>
+      </tr>
+    );
+  };
+
+  return (
+    <div className="flex-1 bg-white p-4 rounded-md m-4 mt-0">
+      {/* TOP SECTION */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All Classes</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          {/* SEARCH */}
+          <TableSearch />
+
+          {/* BUTTONS */}
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-appYellow cursor-pointer">
+              <SlidersHorizontal className="w-4 h-4" />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-appYellow cursor-pointer">
+              <ArrowDownWideNarrow className="w-4 h-4" />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-appYellow cursor-pointer">
+              <Plus className="w-4 h-4" />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* TABLE SECTION */}
+      <Table columns={columns} renderRow={renderRow} data={classesData} />
+
+      {/* PAGINATION */}
+      <Pagination />
+    </div>
+  );
+};
+
+// Export
+export default ClassListPage;
