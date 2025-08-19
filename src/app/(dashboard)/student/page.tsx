6fc3c21f44a941416d282ca870ad6a2c6878c231
Limon00001 <limonhossain82@gmail.com>
2025-08-19 09:32:56 +0600

    feat(student): Implement student dashboard with calendar and announcements features

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -5,11 +5,31 @@
  * @copyright 2025 monayem_hossain_limon
  */
 
+// Internal Imports
+import Announcements from '@/components/Announcements';
+import BigCalendar from '@/components/BigCalendar';
+import EventCalendar from '@/components/EventCalendar';
+
 /**
  * Student Page
  */
 const StudentPage = () => {
-  return <div>StudentPage</div>;
+  return (
+    <div className="flex flex-col xl:flex-row gap-4 p-4">
+      {/* LEFT SIDE */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT SIDE */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 // Export
---
+++ src/components/BigCalendar.tsx
@@ -0,0 +1,53 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 19 Aug, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+// Client Component
+'use client';
+
+// External Imports
+import moment from 'moment';
+import { useState } from 'react';
+import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar';
+
+// Internal Imports
+import { calendarEvents } from '@/lib/data';
+
+// Styles
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+// Instance of Big Calendar
+const localizer = momentLocalizer(moment);
+
+/**
+ * Big Calendar Component
+ */
+const BigCalendar = () => {
+  const [view, setView] = useState<View>(Views.WORK_WEEK);
+
+  // Change View Handler Function
+  const handleOnChangeView = (selectedView: View) => {
+    setView(selectedView);
+  };
+
+  return (
+    <Calendar
+      localizer={localizer}
+      events={calendarEvents}
+      startAccessor="start"
+      endAccessor="end"
+      view={view}
+      views={['work_week', 'day']}
+      style={{ height: '98%' }}
+      onView={handleOnChangeView}
+      min={new Date(2025, 7, 12, 8, 0)}
+      max={new Date(2025, 7, 16, 14, 0)}
+    />
+  );
+};
+
+// Export
+export default BigCalendar;
