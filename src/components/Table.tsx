f26aee0e62d0b0cd89469ada52f2656435476c54
Limon00001 <limonhossain82@gmail.com>
2025-08-19 15:04:21 +0600

    feat(teachers): add teacher listing table and reusable Table component

---
+++ src/components/Table.tsx
@@ -0,0 +1,40 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 19 Aug, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+// External Imports
+import React from 'react';
+
+/**
+ * Table Component
+ */
+const Table = ({
+  columns,
+  renderRow,
+  data,
+}: {
+  columns: { header: string; accessor: string; className?: string }[];
+  renderRow: (item: any) => React.ReactNode;
+  data: any[];
+}) => {
+  return (
+    <table className="w-full mt-4">
+      <thead>
+        <tr className="text-left text-gray-500 text-sm">
+          {columns.map((column) => (
+            <th key={column.accessor} className={column.className}>
+              {column.header}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>{data.map((item) => renderRow(item))}</tbody>
+    </table>
+  );
+};
+
+// Export
+export default Table;
