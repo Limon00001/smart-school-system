f50a27a5964ab126561de9ba63afd9c0cb825e6c
Limon00001 <limonhossain82@gmail.com>
2025-08-19 09:38:56 +0600

    feat(teacher): Implement teacher dashboard

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -5,12 +5,30 @@
  * @copyright 2025 monayem_hossain_limon
  */
 
+// Internal Imports
+import Announcements from '@/components/Announcements';
+import BigCalendar from '@/components/BigCalendar';
+
 /**
- * Techer Page
+ * Teacher Page
  */
-const TecherPage = () => {
-  return <div>TecherPage</div>;
+const TeacherPage = () => {
+  return (
+    <div className="flex-1 flex flex-col xl:flex-row gap-4 p-4">
+      {/* LEFT SIDE */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT SIDE */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 // Export
-export default TecherPage;
+export default TeacherPage;
