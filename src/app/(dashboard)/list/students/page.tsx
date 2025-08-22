7365e3467d95104d111f59dd7bf1382553a2cb8b
Limon00001 <limonhossain82@gmail.com>
2025-08-23 00:01:34 +0600

    feat(students): integrate role-based access feature in students page

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -16,12 +16,18 @@ import FormModal from '@/components/FormModal';
 import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
-import { role } from '@/lib/data';
 import prisma from '@/lib/prisma';
 import { ITEMS_PER_PAGE } from '@/lib/settings';
+import { getCurrentUser } from '@/lib/utils';
 
 // Types
 type StudentList = Student & { class: Class };
+
+// Current User
+const user = await getCurrentUser();
+
+const role = user?.role;
+const userId = user?.userId;
 
 // Data
 const columns = [
@@ -49,10 +55,14 @@ const columns = [
     accessor: 'address',
     className: 'hidden lg:table-cell',
   },
-  {
-    header: 'Actions',
-    accessor: 'action',
-  },
+  ...(role === 'admin'
+    ? [
+        {
+          header: 'Actions',
+          accessor: 'action',
+        },
+      ]
+    : []),
 ];
 
 // Render Table Row
