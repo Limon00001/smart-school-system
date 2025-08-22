b9095d99f4844895f62a8df13814d3890bd05648
Limon00001 <limonhossain82@gmail.com>
2025-08-23 00:02:03 +0600

    feat(parents): integrate role-based access feature in parents page

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -14,14 +14,20 @@ import FormModal from '@/components/FormModal';
 import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
-import { role } from '@/lib/data';
 import prisma from '@/lib/prisma';
 import { ITEMS_PER_PAGE } from '@/lib/settings';
+import { getCurrentUser } from '@/lib/utils';
 
 // Types
 type ParentList = Parent & {
   students: Student[];
 };
+
+// Current User
+const user = await getCurrentUser();
+
+const role = user?.role;
+const userId = user?.userId;
 
 // Data
 const columns = [
@@ -44,10 +50,14 @@ const columns = [
     accessor: 'address',
     className: 'hidden lg:table-cell',
   },
-  {
-    header: 'Actions',
-    accessor: 'action',
-  },
+  ...(role === 'admin'
+    ? [
+        {
+          header: 'Actions',
+          accessor: 'action',
+        },
+      ]
+    : []),
 ];
 
 // Render Table Row
