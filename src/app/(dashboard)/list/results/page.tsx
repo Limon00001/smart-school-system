2ce2101892cb4830867b593d562753a5d0dd9872
Limon00001 <limonhossain82@gmail.com>
2025-08-22 12:04:20 +0600

    feat(results): enhance result list page with data fetching and improved searching & filtering

--- src/app/(dashboard)/list/results/page.tsx
+++ src/app/(dashboard)/list/results/page.tsx
@@ -13,25 +13,29 @@ import FormModal from '@/components/FormModal';
 import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
-import { resultsData, role } from '@/lib/data';
+import { role } from '@/lib/data';
+import prisma from '@/lib/prisma';
+import { ITEMS_PER_PAGE } from '@/lib/settings';
+import { Prisma } from '@prisma/client';
 
 // Types
-type Result = {
-  id: number;
-  subject: string;
-  class: string;
-  teacher: string;
-  student: string;
-  type: 'exam' | 'assignment';
-  date: string;
+type ResultList = {
+  id: string;
+  title: string;
+  studentName: string;
+  studentSurname: string;
+  teacherName: string;
+  teacherSurname: string;
   score: number;
+  className: string;
+  date: Date;
 };
 
 // Data
 const columns = [
   {
-    header: 'Subject Name',
-    accessor: 'name',
+    header: 'Title',
+    accessor: 'title',
   },
   {
     header: 'Student',
@@ -63,44 +67,188 @@ const columns = [
   },
 ];
 
+// Render Table Row
+const renderRow = (item: ResultList) => {
+  return (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-appPurpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">{item.title}</td>
+      <td>{`${item.studentName} ${item.studentSurname}`}</td>
+      <td className="hidden md:table-cell">{item.score}</td>
+      <td className="hidden md:table-cell">{`${item.teacherName} ${item.teacherSurname}`}</td>
+      <td className="hidden md:table-cell">{item.className}</td>
+      <td className="hidden md:table-cell">
+        {new Intl.DateTimeFormat('en-US').format(item.date)}
+      </td>
+      <td>
+        <div className="flex items-center gap-2">
+          {/* <Link href={`/list/teachers/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full text-gray-600 bg-appSky cursor-pointer">
+              <SquarePen className="w-4 h-4" />
+            </button>
+          </Link> */}
+          {role === 'admin' && (
+            <>
+              {/* <button className="w-7 h-7 flex items-center justify-center rounded-full bg-red-500/30 cursor-pointer">
+              <Trash className="w-4 h-4" />
+            </button> */}
+              <FormModal table="result" type="update" data={item} />
+              <FormModal table="result" type="delete" id={item.id} />
+            </>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+};
+
 /**
  * Result List Page
  */
-const ResultListPage = () => {
-  // Render Table Row
-  const renderRow = (item: Result) => {
-    return (
-      <tr
-        key={item.id}
-        className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-appPurpleLight"
-      >
-        <td className="flex items-center gap-4 p-4">{item.subject}</td>
-        <td>{item.student}</td>
-        <td className="hidden md:table-cell">{item.score}</td>
-        <td className="hidden md:table-cell">{item.teacher}</td>
-        <td className="hidden md:table-cell">{item.class}</td>
-        <td className="hidden md:table-cell">{item.date}</td>
-        <td>
-          <div className="flex items-center gap-2">
-            {/* <Link href={`/list/teachers/${item.id}`}>
-              <button className="w-7 h-7 flex items-center justify-center rounded-full text-gray-600 bg-appSky cursor-pointer">
-                <SquarePen className="w-4 h-4" />
-              </button>
-            </Link> */}
-            {role === 'admin' && (
-              <>
-                {/* <button className="w-7 h-7 flex items-center justify-center rounded-full bg-red-500/30 cursor-pointer">
-                <Trash className="w-4 h-4" />
-              </button> */}
-                <FormModal table="result" type="update" data={item} />
-                <FormModal table="result" type="delete" id={item.id} />
-              </>
-            )}
-          </div>
-        </td>
-      </tr>
-    );
-  };
+const ResultListPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
+  // Pagination
+  const { page, ...queryParams } = searchParams;
+
+  const p = page ? parseInt(page) : 1;
+
+  // Query
+  const query: Prisma.ResultWhereInput = {};
+
+  /***
+   * Query Params Filter
+   */
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case 'search':
+            query.OR = [
+              {
+                exam: {
+                  title: {
+                    contains: value,
+                    mode: 'insensitive',
+                  },
+                },
+              },
+              {
+                assignment: {
+                  title: {
+                    contains: value,
+                    mode: 'insensitive',
+                  },
+                },
+              },
+              {
+                student: {
+                  name: {
+                    contains: value,
+                    mode: 'insensitive',
+                  },
+                },
+              },
+              {
+                student: {
+                  surname: {
+                    contains: value,
+                    mode: 'insensitive',
+                  },
+                },
+              },
+            ];
+            break;
+          case 'studentId':
+            query.studentId = value;
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  const [dataResponse, count] = await prisma.$transaction([
+    prisma.result.findMany({
+      where: query,
+      include: {
+        student: {
+          select: {
+            name: true,
+            surname: true,
+          },
+        },
+        exam: {
+          include: {
+            lesson: {
+              select: {
+                class: {
+                  select: {
+                    name: true,
+                  },
+                },
+                teacher: {
+                  select: {
+                    name: true,
+                    surname: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+        assignment: {
+          include: {
+            lesson: {
+              select: {
+                class: {
+                  select: {
+                    name: true,
+                  },
+                },
+                teacher: {
+                  select: {
+                    name: true,
+                    surname: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      take: ITEMS_PER_PAGE,
+      skip: (p - 1) * ITEMS_PER_PAGE,
+    }),
+    prisma.result.count({ where: query }),
+  ]);
+
+  const data = dataResponse.map((item) => {
+    const assesment = item.exam || item.assignment;
+
+    if (!assesment) {
+      return null;
+    }
+
+    const isExam = 'startTime' in assesment;
+
+    return {
+      id: item.id,
+      title: assesment.title,
+      studentName: item.student.name,
+      studentSurname: item.student.surname,
+      teacherName: assesment.lesson.teacher.name,
+      teacherSurname: assesment.lesson.teacher.surname,
+      score: item.score,
+      className: assesment.lesson.class.name,
+      startTime: isExam ? assesment.startTime : assesment.startDate,
+    };
+  });
 
   return (
     <div className="flex-1 bg-white p-4 rounded-md m-4 mt-0">
@@ -130,10 +278,10 @@ const ResultListPage = () => {
       </div>
 
       {/* TABLE SECTION */}
-      <Table columns={columns} renderRow={renderRow} data={resultsData} />
+      <Table columns={columns} renderRow={renderRow} data={data} />
 
       {/* PAGINATION */}
-      <Pagination />
+      <Pagination page={p} count={count} />
     </div>
   );
 };
