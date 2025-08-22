8de49ecb8d9d06eec0a86583bbf86305f0d37252
Limon00001 <limonhossain82@gmail.com>
2025-08-22 23:05:07 +0600

    feat(classes): integrate role-based access feature in class page

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -14,12 +14,18 @@ import FormModal from '@/components/FormModal';
 import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
-import { role } from '@/lib/data';
 import prisma from '@/lib/prisma';
 import { ITEMS_PER_PAGE } from '@/lib/settings';
+import { getCurrentUser } from '@/lib/utils';
 
 // Types
 type ClassList = Class & { supervisor: Teacher };
+
+// Current User
+const user = await getCurrentUser();
+
+const role = user?.role;
+const userId = user?.userId;
 
 // Data
 const columns = [
@@ -42,10 +48,14 @@ const columns = [
     accessor: 'supervisor',
     className: 'hidden md:table-cell',
   },
-  {
-    header: 'Actions',
-    accessor: 'action',
-  },
+  ...(role === 'admin'
+    ? [
+        {
+          header: 'Actions',
+          accessor: 'action',
+        },
+      ]
+    : []),
 ];
 
 // Render Table Row
