cf897174043a4a8a5f6df35bf0e3dfce65b400d4
Limon00001 <limonhossain82@gmail.com>
2025-08-23 22:24:07 +0600

    feat(student): implement user-specific class data fetching and update calendar component

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -7,20 +7,39 @@
 
 // Internal Imports
 import Announcements from '@/components/Announcements';
-import BigCalendar from '@/components/BigCalendar';
+import BigCalendarContainer from '@/components/BigCalendarContainer';
 import EventCalendar from '@/components/EventCalendar';
+import prisma from '@/lib/prisma';
+import { getCurrentUser } from '@/lib/utils';
 
 /**
  * Student Page
  */
-const StudentPage = () => {
+const StudentPage = async () => {
+  // Current User
+  const user = await getCurrentUser();
+
+  // User ID
+  const userId = user?.userId;
+
+  // Classes Data Fetch from the Database
+  const classItem = await prisma.class.findMany({
+    where: {
+      students: {
+        some: {
+          id: userId!,
+        },
+      },
+    },
+  });
+
   return (
     <div className="flex flex-col xl:flex-row gap-4 p-4">
       {/* LEFT SIDE */}
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
           <h1 className="text-xl font-semibold">Schedule (4A)</h1>
-          <BigCalendar />
+          <BigCalendarContainer type="classId" id={classItem[0].id} />
         </div>
       </div>
       {/* RIGHT SIDE */}
